4d8e6d647fbe7d2b4ffc12c10b4b174ad684301f
BrucelD14 <samuderaduta144@gmail.com>
2022-11-17 15:38:52 +0700

    start new project

--- src/App.js
+++ src/App.js
@@ -1,50 +1,5 @@
-import { Button, styled, Typography } from "@mui/material";
-import { Add, Settings } from "@mui/icons-material";
-
 function App() {
-  const BlueButton = styled(Button)({
-    backgroundColor: "skyblue",
-    color: "#888",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "lightblue",
-    },
-    "&:disabled": {
-      backgroundColor: "gray",
-      color: "white",
-    },
-  });
-
-  return (
-    <div>
-      <Button variant="text">Text</Button>
-      <Button
-        startIcon={<Settings />}
-        variant="contained"
-        color="secondary"
-        size="small"
-      >
-        Settings
-      </Button>
-      <Button
-        startIcon={<Add />}
-        variant="contained"
-        color="success"
-        size="small"
-      >
-        Add new post
-      </Button>
-      <Button variant="outlined" disabled>
-        Outlined
-      </Button>
-
-      <Typography variant="h1" component="p">
-        ini menggunakan gaya H1 tapi tag p
-      </Typography>
-      <BlueButton>MY BUTTON</BlueButton>
-      <BlueButton>ANOTHER BUTTON</BlueButton>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
