c41a1909a28e2c4af843e8f353d9378acd577da9
BrucelD14 <samuderaduta144@gmail.com>
2022-11-17 14:14:55 +0700

    learn button and icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import { Add, Settings } from "@mui/icons-material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Settings
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new post
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
     </div>
   );
 }
