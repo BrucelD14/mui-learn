8fd718467ccea915326397f9c5d6b84c82317752
BrucelD14 <samuderaduta144@gmail.com>
2022-11-24 16:03:47 +0700

    create dark mode

--- src/components/Add.js
+++ src/components/Add.js
@@ -58,7 +58,7 @@ const Add = () => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+        <Box width={400} height={280} bgcolor={"background.default"} color={"text.primary"} p={3} borderRadius={5}>
           <Typography variant="h6" color="gray" textAlign="center">
             Create Post
           </Typography>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,7 +19,7 @@ import {
 } from "@mui/material";
 import React from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ mode, setMode }) => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
       <Box position="fixed">
@@ -92,7 +92,9 @@ const Sidebar = () => {
               <ListItemIcon>
                 <DarkMode />
               </ListItemIcon>
-              <Switch />
+              <Switch
+                onChange={(e) => setMode(mode === "light" ? "dark" : "light")}
+              />
             </ListItemButton>
           </ListItem>
         </List>
