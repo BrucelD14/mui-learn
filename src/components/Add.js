b759ccc1480005aff39fbbbf18f700ae8eb036de
BrucelD14 <samuderaduta144@gmail.com>
2022-11-24 13:36:07 +0700

    add content of Add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,15 +1,41 @@
-import { Fab, Tooltip } from "@mui/material";
-import React from "react";
+import { Box, Fab, Modal, styled, Tooltip } from "@mui/material";
+import React, { useState } from "react";
 import { Add as AddIcon } from "@mui/icons-material";
 
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
 const Add = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <Tooltip title="Delete">
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Delete"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
         <Fab color="primary" aria-label="add">
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          Hello
+        </Box>
+      </StyledModal>
     </>
   );
 };
